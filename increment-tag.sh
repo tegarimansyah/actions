ad240b871184abd82792d7876ef73db61e382c83
Tegar Imansyah <egar.imansyah@gmail.com>
2023-09-09 14:28:37 +0000

    new commit

--- increment-tag.sh
+++ increment-tag.sh
@@ -1,13 +1,8 @@
 current_tag=$(git describe --abbrev=0 --tags | grep "hybrid")
 version=$(echo "$current_tag" | grep -oP '\d+\.\d+\.\d+') # Extract the numeric part (e.g., 0.0.5)
 new_version=$(echo "$version" | awk -F. '{$3=$3+1;OFS=".";print $1,$2,$3}') # Increment the minor version (assumes a format like x.y.z)
+new_tag="$new_version-hybrid" # Create the new tag
 
-# Create the new tag
-new_tag="$new_version-hybrid"
-
-# Create a new lightweight tag
 git tag "$new_tag"
-
-# Output the new tag
 echo "Previous tag: $current_tag"
 echo "New tag: $new_tag"
